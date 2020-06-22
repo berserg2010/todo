7011358da6ec9739fb7ec1659029707f27ac6925
berserg2010 <berserg2010@gmail.com>
2020-06-23 00:20:43 +0300

    Add email settings

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
 
     path("api/auth/",  include("rest_framework.urls")),
     # path("api/auth/",  include("djoser.urls.authtoken")),
-    path("api/v1/", include(router.urls)),
+    path("api/", include(router.urls)),
 ]
--- backend/event/views.py
+++ backend/event/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth import get_user_model
+from django.core.mail import send_mail
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
+from backend import settings
 from .models import Event
 from .serializers import EventSerializer
 from .permissions import IsOwner
@@ -19,4 +22,12 @@ class EventViewSet(ModelViewSet):
         return Event.objects.filter(owner=self.request.user)
 
     def perform_create(self, serializer):
+
+        send_mail(
+            f"Новое событие <<{serializer.validated_data.get('title')}>>",
+            f"{serializer.validated_data.get('description')}\n{serializer.validated_data.get('event_date')}",
+            settings.EMAIL_HOST_USER,
+            [ self.request.user.email ],
+        )
+
         serializer.save(owner=self.request.user)
