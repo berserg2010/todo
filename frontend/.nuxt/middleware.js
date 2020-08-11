24a303a5c9dca3b43935e681267e41258f6cf923
berserg2010 <berserg2010@gmail.com>
2020-08-11 03:37:14 +0300

    Fix docker prod

--- frontend/.nuxt/middleware.js
+++
@@ -1,9 +0,0 @@
-const middleware = {}
-
-middleware['auth'] = require('../middleware/auth.js')
-middleware['auth'] = middleware['auth'].default || middleware['auth']
-
-middleware['check-auth'] = require('../middleware/check-auth.js')
-middleware['check-auth'] = middleware['check-auth'].default || middleware['check-auth']
-
-export default middleware
--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -78,4 +78,5 @@ export default {
   */
   build: {
   },
+  srcDir: 'client/',
 }
